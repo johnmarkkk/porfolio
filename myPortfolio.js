9afb929fec54e96f26bb137ae3a4f10c53c078a1
johnmarkkk <johnmarkglomar@gmail.com>
2024-02-20 14:19:22 +0800

    second commit for my portfolio

--- myPortfolio.js
+++ myPortfolio.js
@@ -1,18 +1,22 @@
 let burgerMenu = document.querySelector(".burger");
-let burgerShow = document.querySelector(".burger-menu");
+let burgerShow = document.querySelector(".duplicate");
 let close = document.querySelector("#close");
 
 
 burgerMenu.addEventListener('click', () => {
     burgerShow.style.display = "flex";
+    burgerShow.style.right = 0;
+    burgerMenu.style.display = "none";
 });
 
 close.addEventListener('click', () => {
-    burgerShow.style.display = "none";
+    burgerShow.style.right = "-250px";
+    burgerMenu.style.display = "flex";
 })
 
 window.addEventListener('resize', () => {
     if (window.innerWidth >= 549) {
         burgerShow.style.display = "none";
+        burgerMenu.style.display = "flex";
     }
 })
